b830e0d377d88e5388d0d9091a98a4b760925dda
hzx19980513 <923450872@qq.com>
2023-11-10 21:32:11 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   // https://go.nuxtjs.dev/config-axios
   axios: {
     // https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://xxx.xxx.xxx/'
+    baseURL: 'http://106.225.164.193:2000/'
   },
 
   // https://go.nuxtjs.dev/pwa
